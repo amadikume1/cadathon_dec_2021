c761780c12dd4fa454036fca176d36a6ef207104
kdundigalla25 <93355579+kdundigalla25@users.noreply.github.com>
2021-12-20 12:45:39 -0500

    Add files via upload

---
+++ team_7/Test Command Robot/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  /** Creates a new DrivetrainSubsystem. */
+  WPI_TalonFX leftTalon1, leftTalon2, rightTalon1, rightTalon2;
+  public DrivetrainSubsystem() {
+    leftTalon1 = new WPI_TalonFX(1);
+    leftTalon2 = new WPI_TalonFX(2);
+    rightTalon1 = new WPI_TalonFX(3);
+    rightTalon2 = new WPI_TalonFX(4);
+    leftTalon1.setInverted(true);
+    leftTalon2.setInverted(true);
+  }
+  
+
+  public void drive(double leftPower,double rightPower ) {
+    leftTalon1.set(-leftPower);
+    leftTalon2.set(-leftPower);
+    rightTalon1.set(-rightPower);
+    rightTalon2.set(-rightPower);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
